92d59f8634f0cc111aefa7d7fff34cd249a60f48
FaridClaros <faridclaros15@gmail.com>
2025-02-04 01:23:13 -0600

    Calculadora con multiples operaciones

--- app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.miprimeraaplicacion;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -14,6 +17,8 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     Button btn;
     TextView tempVal;
+    RadioGroup rgb;
+    RadioButton opt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +32,36 @@ public class MainActivity extends AppCompatActivity {
                 double num1 = Double.parseDouble(tempVal.getText().toString());
                 tempVal = findViewById(R.id.txtNum2);
                 double num2 = Double.parseDouble(tempVal.getText().toString());
-                double respuesta = num1 + num2;
+
+                double respuesta = 0.0;
+
+                opt = findViewById(R.id.optSuma);
+                if (opt.isChecked()) {
+                    respuesta = num1 + num2;
+                }
+                opt = findViewById(R.id.optResta);
+                if (opt.isChecked()) {
+                    respuesta = num1 - num2;
+                }
+                opt = findViewById(R.id.optMultiplicacion);
+                if (opt.isChecked()) {
+                    respuesta = num1 * num2;
+                }
+                opt = findViewById(R.id.optDivision);
+                if (opt.isChecked()) {
+                    respuesta = num1 / num2;
+                }
+                opt = findViewById(R.id.optExponenciacion);
+                if (opt.isChecked()){
+                    respuesta = Math.pow(num1,num2);
+                }
+
+                opt =findViewById(R.id.optRaiz);
+                if (opt.isChecked()){
+                    respuesta = Math.sqrt(num1);
+                    respuesta = Math.sqrt(num2);
+                }
+
                 tempVal = findViewById(R.id.lblRespuesta);
                 tempVal.setText("Respuesta: "+ respuesta);
             }
