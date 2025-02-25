02718cfe383f2e28c8d32304b95755ff06b302ce
FaridClaros <faridclaros15@gmail.com>
2025-02-25 10:04:47 -0600

    avance del parcial

--- app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
@@ -29,13 +29,57 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TabHost tbh;
+    TextView tempVal;
+    Spinner spn;
+    Button btn;
+    conversor objconversor = new conversor();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tbh = findViewById(R.id.tbhConversor);
+        tbh.setup();
+
+        tbh.addTab(tbh.newTabSpec("Monedas").setContent(R.id.tabAreas).setIndicator("AREAS", null));
+        tbh.addTab(tbh.newTabSpec("Longitud").setContent(R.id.tabTarifa).setIndicator("TARIFA", null));
+
+        btn = findViewById(R.id.btnCalcular);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int opcion = tbh.getCurrentTab();
+
+                spn = findViewById(R.id.spnDeAreas);
+                int de = spn.getSelectedItemPosition();
+
+                spn = findViewById(R.id.spnAAreas);
+                int a = spn.getSelectedItemPosition();
+
+                tempVal = findViewById(R.id.txtCantidad);
+                double cantidad = Double.parseDouble(tempVal.getText().toString());
+
+
+                tempVal = findViewById(R.id.lblRespuesta);
+                double respuesta = objconversor.convertir(opcion, de, a, cantidad);
+                tempVal.setText("Respuesta: "+ respuesta);;
+            }
+        });
+
     }
+    class conversor {
+        double[][] valores = {
+                {1, 0.006944, 0.1111, 0.092903, 0.000031, 0.000007, 0.00000929},//piecuadrado
+        };
+
+        public double convertir(int opcion, int de, int a, double cantidad) {
+            return valores[opcion][a] / valores[opcion][de] * cantidad;
+        }
+    }
+
 
 }
 
