c85744ce605539b83cca30fe9166133c0bddef0d
FaridClaros <faridclaros15@gmail.com>
2025-02-25 01:17:20 -0600

    creacion de la rama gps y su funcionamiento

--- app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.miprimeraaplicacion;
 
+import android.Manifest;
 import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +21,7 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -24,61 +29,48 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     TextView tempVal;
-    SensorManager sensorManager;
-    Sensor sensor;
-    SensorEventListener sensorEventListener;
+    LocationManager locationManager;
+    LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        sensorLuz();
+        tempVal = findViewById(R.id.lblSensorGps);
+        obtenerPosicion();
     }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        iniciar();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        detener();
-    }
-
-    private void iniciar(){
-        sensorManager.registerListener(sensorEventListener, sensor, 2000*1000);
-    }
-
-    private void detener(){
-        sensorManager.unregisterListener(sensorEventListener);
-    }
-
-    private void sensorLuz(){
-        tempVal = findViewById(R.id.lblSensorAcelerometro);
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        if (sensor ==null){
-            tempVal.setText("Tu dispositivo, no tiene el sensor de acelerometro");
-            finish();
-        }
-        sensorEventListener = new SensorEventListener() {
-            @Override
-            public void onSensorChanged(SensorEvent event) {
-                double x = event.values[0];
-                double y = event.values[1];
-                double z = event.values[2];
-                tempVal.setText("Desplazamiento X= "+ x + "; Y="+y + "; Z="+z);
-
-
+    void obtenerPosicion(){
+        try{
+            locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+            if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != getPackageManager().PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != getPackageManager().PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                tempVal.setText("Solicitando permisos de ubicación...");
             }
 
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int i) {
-
-            }
-        };
+            locationListener = new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    mostrarUbicacion(location);
+                }
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+                    tempVal.setText("Estado del proveedor: "+ status);
+                }
+                @Override
+                public void onProviderEnabled(String provider) {
+                    tempVal.setText("Proveedor habilitado: "+ provider);
+                }
+                @Override
+                public void onProviderDisabled(String provider) {
+                    tempVal.setText("Proveedor deshabilitado: "+ provider);
+                }
+            };
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }catch (Exception e){ tempVal.setText("Error al obtener la ubicación:"+e.getMessage());}
     }
+    void mostrarUbicacion(Location location){
+        tempVal.setText("Latitud: "+ location.getLatitude() + "\nLongitud: "+ location.getLongitude() + "\nAltitud: "+ location.getAltitude());
+    }
+
 }
